5ea027c1d8f6af9345148103c8d5549b18e6d6ae
dertex909 <codshannw@gmail.com>
2026-07-21 15:30:18 +0500

    Добавляю лицензионные заголовки и оптимизирую импорты в CabinBuilder.

--- src/main/java/org/complexityanalyzer/util/FluidNormalizer.java
+++ src/main/java/org/complexityanalyzer/util/FluidNormalizer.java
@@ -1,11 +1,30 @@
+/*
+ * Complexity Analyzer
+ * Copyright (C) 2025-2026 dertex909
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.complexityanalyzer.util;
 
-import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
 
 public final class FluidNormalizer {
-    private FluidNormalizer() {}
+    private FluidNormalizer() {
+    }
 
     public static Fluid normalize(Fluid fluid) {
         if (fluid == null) return Fluids.EMPTY;

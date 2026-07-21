b0b73a2c884da395b0a772a3fdb17cf45da4abb1
dertex909 <codshannw@gmail.com>
2026-07-21 22:16:28 +0500

    Удаляю неиспользуемый эвристический сборщик пользовательских рецептов.

--- src/main/java/org/complexityanalyzer/harvest/RegistryHarvestService.java
+++ src/main/java/org/complexityanalyzer/harvest/RegistryHarvestService.java
@@ -103,9 +103,6 @@ public final class RegistryHarvestService {
 
         debugTrace.flush();
         DynamicRecipeHarvester.harvest(graph, level, knownRecipeIds);
-
-        //test
-        HeuristicCustomRecipeHarvester.harvest(graph, level);
         harvester.clearCaches();
 
         ComplexityAnalyzer.LOGGER.info("[Harvest] Runtime scan complete: {} scanned, {} harvested, {} rejected, {} failed",
